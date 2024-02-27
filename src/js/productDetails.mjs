1252eec83ad0ddf8eb2a174c9099eb7aad2618b7
Kathryn Lambert <lam20005@byui.edu>
2024-02-27 14:03:23 -0700

    animate backpack when add to cart

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,17 +1,17 @@
 import { findProductById } from "./productData.mjs";
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { cartCount } from "./stores.mjs";
+import runAnimateCart from "./addToCart.js";
 
-let product = {}
+let product = {};
 
-
-// productDetails is just bringing over the html structure and elements so that when we add the productDetailsTemplate it adds all the content. 
+// productDetails is just bringing over the html structure and elements so that when we add the productDetailsTemplate it adds all the content.
 export default async function productDetails(productId, selector) {
-    // let product = await makeRequest(baseUrl + 'productID')
-    product = await findProductById(productId);
-    const el = document.querySelector(selector);
-    el.insertAdjacentHTML('afterbegin', productDetailsTemplate(product));
-    document.getElementById("addToCart").addEventListener("click", addToCart);
+  // let product = await makeRequest(baseUrl + 'productID')
+  product = await findProductById(productId);
+  const el = document.querySelector(selector);
+  el.insertAdjacentHTML("afterbegin", productDetailsTemplate(product));
+  document.getElementById("addToCart").addEventListener("click", addToCart);
 }
 
 function addToCart() {
@@ -25,10 +25,11 @@ function addToCart() {
   setLocalStorage("so-cart", cartContents);
   // update the visible cartCount
   cartCount.set(cartContents.length);
+  runAnimateCart();
 }
 
-export function productDetailsTemplate(product){
-   return `<h3>${product.Brand.Name}</h3>
+export function productDetailsTemplate(product) {
+  return `<h3>${product.Brand.Name}</h3>
 
    <h2 class="divider">${product.NameWithoutBrand}</h2>
 
@@ -48,6 +49,6 @@ export function productDetailsTemplate(product){
    <div class="product-detail__add">
      <button id="addToCart" data-id='${product.Id}'>Add to Cart</button>
    </div> 
-   `
-// insert the product specifics into a string of markup.
+   `;
+  // insert the product specifics into a string of markup.
 }

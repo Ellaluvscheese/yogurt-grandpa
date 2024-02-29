1a480bea486c7c57aed66e54b0d2a92ce20231a7
brinleylott <bvg56010@gmail.com>
2024-02-29 14:22:17 -0700

    finishing adding the text

--- src/js/components/ProductSummary.svelte
+++ src/js/components/ProductSummary.svelte
@@ -8,5 +8,6 @@
       <h3 class="card__brand">{product.Brand.Name}</h3>
       <h2 class="card__name">{product.NameWithoutBrand}</h2>
       <p class="product-card__price">${product.FinalPrice}</p>
+      <p class="discount">EXTRA 25% OFF AT CHECK OUT</p>
     </a>
 </section>
